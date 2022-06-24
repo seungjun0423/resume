4e5413e61c98d87d74169c7f523b9f304cb1bd06
seungjun0423 <sibonum@gmail.com>
2022-06-25 03:53:42 +0900

    [FEAT] Add Structer of client directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          안녕하세요
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          프론트엔드 엔지니어 이승준입니다
       </header>
     </div>
   );
