8aa24b9cd335521155ae2e4437a90ed902a9643e
seungjun0423 <sibonum@gmail.com>
2022-06-25 13:10:48 +0900

    [FEAT] Add picture to App.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <p>
           안녕하세요
         </p>
           프론트엔드 엔지니어 이승준입니다
+          <img src='../public/접수용.jpg'></img>
       </header>
     </div>
   );
